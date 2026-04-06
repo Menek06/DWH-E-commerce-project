af102c58a54673b545b4bcdd1dc7babc5afbb004
Kacper <miszczakkacperr@gmail.com>
2026-04-06 21:56:59 +0200

    Created all remains tables for bronze layer

--- bronze_layer_ingestion.sql
+++ bronze_layer_ingestion.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA IF NOT EXISTS bronze;
 -- Creating and ingesting data to customers table --
 ----------------------------------------------------
 
-DROP TABLE IF EXISTS bronze.customers;
+DROP TABLE IF bronze.customers;
 CREATE TABLE IF NOT EXISTS bronze.customers(
 	customer_id VARCHAR,
 	first_name VARCHAR,
@@ -16,17 +16,16 @@ CREATE TABLE IF NOT EXISTS bronze.customers(
 	registration_date VARCHAR
 );
 
-COPY bronze.customers(customer_id, first_name, last_name, cust_email, phone, city , country, registration_date)
+COPY bronze.customers
 FROM '/DWH_PROJECT/source/customers.csv'
 DELIMITER ','
 CSV HEADER;
-
 
 ------------------------------------------------------
 -- Creating and ingesting data to order items table --
 ------------------------------------------------------
 
-DROP TABLE IF EXISTS bronze.order_items;
+DROP TABLE IF bronze.order_items;
 CREATE TABLE IF NOT EXISTS bronze.order_items(
 	item_id varchar,
 	order_id VARCHAR,
@@ -40,5 +39,68 @@ COPY bronze.order_items
 FROM '/DWH_PROJECT/source/order_items.csv'
 DELIMITER ','
 CSV HEADER;
+
+--------------------------------------------------
+-- Creating and ingesting data to orders table --
+--------------------------------------------------
+
+DROP TABLE IF EXISTS bronze.orders;
+CREATE TABLE IF NOT EXISTS bronze.orders(
+	order_id VARCHAR,
+	customer_id VARCHAR,
+	order_date VARCHAR,
+	status VARCHAR,
+	shipping_address VARCHAR,
+	shipping_city VARCHAR,
+	shipping_country VARCHAR,
+	total_amount VARCHAR
+);
+
+COPY bronze.orders
+FROM '/DWH_PROJECT/source/orders.csv'
+DELIMITER ','
+CSV HEADER;
+
+----------------------------------------------------
+-- Creating and ingesting data to payments table --
+----------------------------------------------------
+
+DROP TABLE IF EXISTS bronze.payments;
+CREATE TABLE IF NOT EXISTS bronze.payments(
+	payment_id VARCHAR,
+	order_id VARCHAR,
+	payment_date VARCHAR,
+	payment_method VARCHAR,
+	amount VARCHAR,
+	transaction_id VARCHAR,
+	status VARCHAR
+);
+
+COPY bronze.payments
+FROM '/DWH_PROJECT/source/payments.csv'
+DELIMITER ','
+CSV HEADER;
+
+----------------------------------------------------
+-- Creating and ingesting data to products table --
+----------------------------------------------------
+
+DROP TABLE IF EXISTS bronze.products;
+CREATE TABLE IF NOT EXISTS bronze.products(
+	product_id VARCHAR,
+	product_name VARCHAR,
+	category VARCHAR,
+	price VARCHAR,
+	stock VARCHAR,
+	weight_kg VARCHAR,
+	description VARCHAR,
+	created_at VARCHAR
+);
+
+COPY bronze.products
+FROM '/DWH_PROJECT/source/products.csv'
+DELIMITER ','
+CSV HEADER;
+
 
 
