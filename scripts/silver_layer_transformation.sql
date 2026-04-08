1696e52fbb2236cf27f2368de4da72eb1097f629
Kacper <miszczakkacperr@gmail.com>
2026-04-08 23:28:55 +0200

    Make silver_layer_transformation file which contains for now only cleaning and validating customer table. I will tommorow remove duplicates and make a silver layer schema

---
+++ scripts/silver_layer_transformation.sql
@@ -0,0 +1,16 @@
+-- Transforming customer table, validating data types, cleaning white spaces, replaceing missing data 
+SELECT 
+	customer_id::int,
+	TRIM(INITCAP(first_name)) AS first_name,
+	TRIM(INITCAP(last_name)) AS last_name,
+	COALESCE(email, 'N/A') AS email,
+	COALESCE(phone, 'N/A') as phone,
+	TRIM(INITCAP(city)) AS city,
+	INITCAP(LOWER(TRIM(country))) AS country,
+	CASE 
+		WHEN POSITION('/' IN registration_date) > 0 THEN TO_DATE(registration_date, 'DD/MM/YYYY')
+		WHEN POSITION('/' IN registration_date) = 0 THEN TO_DATE(registration_date, 'YYYY-MM-DD')
+	END AS registration_date
+FROM bronze.customers AS c;
+
+--Select  * FROM bronze.customers
